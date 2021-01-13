ed9386d12cff63d7d3b79c3f27474d4d520c52a8
Александр ДОМ <Sanek.s88@gmail.com>
2021-01-13 23:20:23 +0300

    13.01.21/2

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from './App'
 import store from './store'
 import router from './router'
 import Vuetify from 'vuetify'
+import BuyModalComponent from '@/components/Shared/BuyModal'
 import firebase from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
-
+Vue.component('app-buy-modal', BuyModalComponent)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
