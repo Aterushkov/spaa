c1eae4dcb1b1ef2145037bd42917128f9acf48a8
Александр ДОМ <Sanek.s88@gmail.com>
2021-01-14 22:06:01 +0300

    finish

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,7 +1,16 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
+      <v-flex xs12 class="text-xs-center" v-if="loading">
+        <v-progress-circular
+          :size="100"
+          :width="7"
+          color="green"
+          indeterminate
+          class="v-progress-circular"
+        ></v-progress-circular>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
         <h1 class="text--secondary mb-3">Orders</h1>
         <v-list two-line subheader>
           <v-list-tile
@@ -29,36 +38,35 @@
           </v-list-tile>
         </v-list>
       </v-flex>
+      <v-flex xs12 class="text-xs-center" v-else="loading">
+        <h1 class="text--primary">У Вас нет заказаов</h1>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      orders: [
-        {
-          id: 'fds3',
-          name: 'Vladilen',
-          phone: '8-921-121-12-12',
-          adId: '123',
-          done: false
-        },
-        {
-          id: 'ff4',
-          name: 'Nono',
-          phone: '8-921-121-12-12',
-          adId: '1233',
-          done: false
-        }
-      ]
+
+  computed: {
+    loading () {
+      return this.$store.getters.loading
+    },
+    orders () {
+      return this.$store.getters.orders
     }
   },
   methods: {
     markDone (order) {
-      order.done = true
+      this.$store.dispatch('markOrderDone', order.id)
+        .then(() => {
+          order.done = true
+        })
+        .catch(() => {})
     }
+  },
+  created() {
+    this.$store.dispatch('fetchOrders')
   }
 }
 </script>
